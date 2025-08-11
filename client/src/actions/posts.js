fe6e4a00144de6e2e79e89b6988d20695ff2666e
Lalit Mohan <lalitmohan@Hawks-iMac.local>
2025-08-11 15:48:07 +0530

    fixed the post issue

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,22 @@
-import * as api from "../api";
+import * as api from "../api/index.js";
+import { CREATE,FETCH_ALL } from "../constants/actionTypes.js";
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
-    console.log("SRC -> action -> posts.js",error.message);
+    console.log("ACTION -> posts.js -> getPosts(Error) : ", error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  console.log("ACTIONS -> posts.js -> Post in createPost : ", post);
+  try {
+    const { data } = await api.createPost(post);
+    console.log("ACTIONS -> posts.js -> createPost : ", data);
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log("ACTIONS -> post.js -> createPost(Error) : ", error);
   }
 };
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,5 @@ const url = "http://localhost:8080/posts";
 export const fetchPosts = async () => {
   const data = await axios.get(url);
 };
+
+export const createPost = (newPost) =>  axios.post(url, newPost);
