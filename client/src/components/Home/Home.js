9a3098cf47f1e5ab594ffd9ca5e6daf7148bff96
viraat0700 <viraat.shrivastava0700@gmail.com>
2025-09-09 18:31:39 +0530

    feat: add pagination component and styles, update Home component layout
    - Added @material-ui/lab dependency for pagination.
    - Updated API base URL to localhost for development.
    - Enhanced Home component to include pagination functionality.
    - Created new Pagination component for handling post pagination.
    - Added styles for pagination and Home component layout adjustments.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 import { getPosts } from "../../actions/posts.js";
 import { useDispatch } from "react-redux";
 import Posts from "../Posts/Posts.js";
 import Form from "../Form/Form.js";
+import Pagination from "../Pagination.jsx";
+import useStyles from "./styles.js";
 
 const Home = () => {
+  const classes = useStyles();
   const [currentId, setCurrentId] = useState(null);
 
   const dispatch = useDispatch();
@@ -27,6 +30,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper className={classes.pagination} elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
